c89f8b32f58e874be6f1b2b2ebede50743005188
Kalpana Madhalam <mkalpanais@gmail.com>
2017-10-06 20:56:24 +1100

    Refactored redux reducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,63 +1,8 @@
-import {
-  ADD_RECIPE,
-  REMOVE_FROM_CALENDAR,
-} from "../actions";
 import { combineReducers } from 'redux';
-
-const foodReducer = (state={}, action) => {
-  switch(action.type) {
-    case ADD_RECIPE:
-      const { recipe } = action;
-      return {
-        ...state,
-        [recipe.label]: recipe,
-      };
-    default:
-      return state;
-  }
-};
-
-const INITIAL_MENU = {
-  breakfast: null,
-  lunch: null,
-  dinner: null,
-};
-
-const initialCalendarState = {
-  sunday: { ...INITIAL_MENU },
-  monday: { ...INITIAL_MENU },
-  tuesday: { ...INITIAL_MENU },
-  wednesday: { ...INITIAL_MENU },
-  thursday: { ...INITIAL_MENU },
-  friday: { ...INITIAL_MENU },
-  saturday: { ...INITIAL_MENU },
-};
-
-const calendarReducer = (state=initialCalendarState, action) => {
-  const { day, recipe, meal } = action;
-  switch(action.type) {
-    case ADD_RECIPE:
-      return {
-        ...state,
-        [day]: {
-          ...state[day],
-          [meal]: recipe.label,
-        }
-      };
-    case REMOVE_FROM_CALENDAR:
-      return {
-        ...state,
-        [day]: {
-          ...state[day],
-          [meal]: null,
-        }
-      };
-    default:
-      return state;
-  }
-};
+import calendar from './calendar';
+import food from './food';
 
 export default combineReducers({
-  calendar: calendarReducer,
-  food: foodReducer,
+  calendar,
+  food,
 });
